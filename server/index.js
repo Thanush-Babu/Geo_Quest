6bab910faf9da07be906693d1c61fbdfb4cdac45
Thanush Babu <tbabu@fleetstudio.com>
2024-10-06 19:44:21 +0530

    fix: cors

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
 app.use(express.json());
 const dbConfig = require("./config/dbConfig");
@@ -8,7 +9,7 @@ const usersRoute = require("./routes/usersRoute");
 const examsRoute = require("./routes/examsRoute");
 const resportsRoute = require("./routes/reportsRoute");
 
-
+app.use(cors());
 app.use("/api/users", usersRoute);
 app.use("/api/exams", examsRoute);
 app.use("/api/reports", resportsRoute);
